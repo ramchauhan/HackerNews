1dade1f0c9c758eb972d993fcdbbf3726cc60150
ramchauhan <ramawadhchauhan@gmail.com>
2016-08-23 23:54:14 +0530

    Improve performanace.

--- algo_app/fib_calculation.py
+++ algo_app/fib_calculation.py
@@ -11,15 +11,17 @@ class FibCalculator(object):
         :return:
             Nth number of the fibonacci series
         """
-        return list(self._fib_series_gen())[self.num - 1]
+        return self._fib_series_gen()
 
     def _fib_series_gen(self):
         """
-        this will give the nth number of the Fibonacci series which will start from [1, 1, ....]
+        this will generate the nth and nth - 1 elements of fib sequence and return the nth elements of fibonacci series
         :return:
             Nth number of the fibonacci series
         """
         a, b = 1, 1
-        for _ in range(self.num + 1):
-            yield a
+        for _ in xrange(self.num - 1):
+            # updating the nth and nth - 1 elements of the Fibonacci sequence
             a, b = b, a + b
+        # returning nth element
+        return a
